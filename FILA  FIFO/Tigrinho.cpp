f7445b874c420c46e3381f761ecf26b9931e2a46
PauloAthur <arthurking.pereira@gmail.com>
2025-11-05 19:54:20 -0300

    a

---
+++ FILA  FIFO/Tigrinho.cpp
@@ -0,0 +1,15 @@
+// FILA | FIFO
+// FIRST IN, First OUT
+
+#include <iostream>
+#include <string>
+#include <queue> // Contem estrutura fila 
+using namespace std;
+
+int main(){
+    //criação da fila
+    queue<string> fila_sorteio;
+    
+    cout << "Sistema de sorteio";
+    
+}
